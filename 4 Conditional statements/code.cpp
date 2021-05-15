d9afe3a3a70686e1cae4e6df84255f72be0aa79b
LeHoangAn <75709930+lehoangan2906@users.noreply.github.com>
2021-05-15 10:58:09 -0700

    Add files via upload

---
+++ 4 Conditional statements/code.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int i;
+    cin >> i;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    // Write Your Code Here
+    if (i == 1) { cout << "one"; }
+    else if (i == 2) { cout << "two"; }
+    else if (i == 3) { cout << "three"; }
+    else if (i == 4) { cout << "four"; }
+    else if (i == 5) { cout << "five"; }
+    else if (i == 6) { cout << "six"; }
+    else if (i == 7) { cout << "seven"; }
+    else if (i == 8) { cout << "eight"; }
+    else if (i == 9) { cout << "nine"; }
+    else { cout << "Greater than 9"; }
+    return 0;
+}
